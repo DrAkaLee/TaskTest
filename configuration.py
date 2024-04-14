6bd04949eb764cb476a3ef6cab07eb8686868a96
Pavel <p.23nqa@gmail.com>
2024-04-14 20:25:49 +0300

    мое тестовое задание

--- configuration.py
+++ configuration.py
@@ -1,2 +1,2 @@
 SERVICE_URL_GENDER = "https://hr-challenge.dev.tapyou.com/api/test/users?gender=male"
-SERVICE_URL_ID = "https://hr-challenge.dev.tapyou.com/api/test/use1212r"
+SERVICE_URL_ID = "https://hr-challenge.dev.tapyou.com/api/test/user"
