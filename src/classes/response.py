a68c9177a19773446f3613724de28b8a705ad9e2
Pavel <160114595+DrAkaLee@users.noreply.github.com>
2024-04-14 21:33:56 +0300

    Update response.py

--- src/classes/response.py
+++ src/classes/response.py
@@ -1,25 +1,21 @@
 from jsonschema import validate
-
 from src.enums.global_enums import GlobalErrorMessages
 
-
 class Response:
-
     def __init__(self, response):
         self.response = response
         self.response_json = response.json()
         self.response_status = response.status_code
         self.response_gender_male = self.response_json.get
 
-
-    def validate(self, schema):
+    def validate(self, schema):    # Метод для валидации ответа с использованием JSON схемы
         if isinstance(self.response_json, list):
             for item in self.response_json:
                 validate(item, schema)
         else:
             validate(self.response_json, schema)
 
-    def assert_status_code(self, status_code):
+    def assert_status_code(self, status_code):    # Метод для утверждения статуса кода ответа
         if isinstance(status_code, list):
             assert self.response_status in status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
         else:
